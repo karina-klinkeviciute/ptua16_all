67929d7e1c826c200568361b1ef8e81088c45fc1
Karina Klinkevičiūtė <karina.klinkeviciute@gmail.com>
2024-11-20 19:59:28 +0200

    added check_positive

---
+++ karina.py
@@ -0,0 +1,6 @@
+def check_positive(number):
+    """check if number is positive"""
+    if number >= 0:
+        # if it's more than zeoro or zero, it's considered positive
+        return True
+    return False
